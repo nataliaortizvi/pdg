816b07684da49a30035141814612b0a3421c2e13
Natalia Ortiz <60743531+nataliaortizvi@users.noreply.github.com>
2022-10-26 15:39:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutUsView from '../views/AboutUsView.vue'
+import ComunityView from '../views/ComunityView.vue'
+import AnalysisView from '../views/AnalysisView.vue'
+import PapersView from '../views/PapersView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/nosotros',
+      name: 'nosotros',
+      component: AboutUsView
+    },
+    {
+      path: '/comunidad',
+      name: 'comunidad',
+      component: ComunityView
+    },
+    {
+      path: '/analisis',
+      name: 'analisis',
+      component: AnalysisView
+    },
+    {
+      path: '/papers/:varId',
+      name: 'paper',
+      component: PapersView
+    },
+  ]
+})
+
+export default router
